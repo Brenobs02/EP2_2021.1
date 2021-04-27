5dab3d5a16319e12e9e95341b2dbde3cb21122eb
henriquedamico <henriquedamico@hotmail.com>
2021-04-27 16:10:22 -0300

    lista_mov

--- lista_mov.py
+++ lista_mov.py
@@ -1,7 +1,7 @@
 def lista_movimentos_possiveis(baralho, i):
-    movimentos = []
+    mov = []
     if extrai_valor(baralho[i]) == extrai_valor(baralho[i - 1]) or extrai_naipe(baralho[i]) == extrai_naipe(baralho[i - 1]):
-        movimentos.append(1)
+        mov.append(1)
     if extrai_valor(baralho[i]) == extrai_valor(baralho[i - 3]) or extrai_naipe(baralho[i]) == extrai_naipe(baralho[i - 3]):
-        movimentos.append(3)
-    return movimentos
+        mov.append(3)
+    return mov
